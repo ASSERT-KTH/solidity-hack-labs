9c01be86ec07da98f689a224668cf12b8143bc57
Monica Jin <monicachenjin@gmail.com>
2024-06-19 14:26:15 +0200

    add: exploit for arbitrary_location_write_simple.sol

---
+++ smartbugs-curated/0.4.x/test/access_control/arbitrary_location_write_simple_test.js
@@ -0,0 +1,24 @@
+const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
+const { expect } = require('chai');
+
+describe('attack access_control/arbitrary_location_write_simple.sol', function () {
+    async function deployContracts() {
+      const Wallet = await ethers.getContractFactory('contracts/dataset/access_control/arbitrary_location_write_simple.sol:Wallet');
+      const victim = await Wallet.deploy();  
+      await victim.waitForDeployment();
+      const address = await victim.getAddress();
+
+      const WalletAttacker = await ethers.getContractFactory('contracts/access_control/arbitrary_location_write_simple_attack.sol:WalletAttacker');
+      const attacker = await WalletAttacker.deploy(address);  
+      await attacker.waitForDeployment();
+      return {victim, attacker};
+    }
+
+  
+    it('exploit access control vulnerability', async function () {
+      const {victim, attacker} = await loadFixture(deployContracts);
+      await expect(attacker.test(10, 42)).to.be.reverted;
+      await attacker.attack();
+      await expect(attacker.test(10, 42)).to.not.be.reverted;
+    });
+  });
