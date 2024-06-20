6c2461de1c4cd8da4ad09b0cf0ce49674b61a716
sofiabobadilla <sofy.bobadilla@gmail.com>
2024-06-20 14:56:40 +0200

    fix: removing debugging code from simpledao.js

--- smartbugs-curated/0.4.x/test/reentrancy/simple_dao.js
+++ smartbugs-curated/0.4.x/test/reentrancy/simple_dao.js
@@ -25,8 +25,6 @@ describe("Reentrancy Attack for simpleDAO.sol", function () {
     const {simpleDAO, maliciousContract} = await loadFixture(deployContracts);
     const [_, innocentAddress, attackerAddress] = [,simpleDAO.target, maliciousContract.target];
 
-    console.log(innocentAddress);
-    console.log(attackerAddress);
 
     // We add 10 ether into victim contract
     await simpleDAO.donate( innocentAddress, {
@@ -45,8 +43,8 @@ describe("Reentrancy Attack for simpleDAO.sol", function () {
       });
 
     // Check balances after attack
-    const maliciousContractBalance = await ethers.provider.getBalance(maliciousContract.target);
-    const simpleDAOBalance = await ethers.provider.getBalance(simpleDAO.target);
+    const maliciousContractBalance = await ethers.provider.getBalance(attackerAddress);
+    const simpleDAOBalance = await ethers.provider.getBalance(innocentAddress);
 
     // Verify the attack was successful
     expect(maliciousContractBalance).to.equal(ethers.parseEther("11")); //11= 1 from the donation, 10 from the attack
