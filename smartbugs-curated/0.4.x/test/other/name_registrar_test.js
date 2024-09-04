7312c6e1bbda789ea33afc36797969ef1efe86a2
Monica Jin <monicachenjin@gmail.com>
2024-09-04 11:52:43 +0200

    add name_registrar exploit

---
+++ smartbugs-curated/0.4.x/test/other/name_registrar_test.js
@@ -0,0 +1,30 @@
+const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
+const { expect } = require('chai');
+
+describe('attack other/name_registrar.sol', function () {
+    async function deployContracts() {
+      const NameRegistrar = await ethers.getContractFactory('contracts/dataset/other/name_registrar.sol:NameRegistrar');
+      const victim = await NameRegistrar.deploy();  
+      await victim.waitForDeployment();
+      const address = await victim.getAddress();
+
+      const NameRegistrarAttacker = await ethers.getContractFactory('contracts/other/name_registrar_attack.sol:NameRegistrarAttacker');
+      const attacker = await NameRegistrarAttacker.deploy(address);
+      await attacker.waitForDeployment();
+      return {victim, attacker};
+    }
+
+  
+    it('exploit access control vulnerability', async function () {
+      const {victim, attacker} = await loadFixture(deployContracts);
+
+      const unlocked = await victim.unlocked();
+      expect(unlocked).to.be.false;
+
+      await attacker.attack();
+
+      const unlockedAfter = await victim.unlocked();
+      expect(unlockedAfter).to.be.true;
+        
+    });
+  });
