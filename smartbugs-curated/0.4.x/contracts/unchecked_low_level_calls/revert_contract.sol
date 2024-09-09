29e3ae6cab9e9d13dfa8b2fe5fb373f49c1a46fd
Monica Jin <monicachenjin@gmail.com>
2024-09-09 20:35:54 +0200

    add exploit

--- smartbugs-curated/0.4.x/contracts/unchecked_low_level_calls/revert_contract.sol
+++ smartbugs-curated/0.4.x/contracts/unchecked_low_level_calls/revert_contract.sol
@@ -1,9 +1,16 @@
 pragma solidity 0.4.25;
 
+import "hardhat/console.sol";
+
 contract RevertContract {
 
     // Fallback function that will fail on purpose
     function() external payable {
         revert("I always revert!");
+    }
+
+    function sendEther(address _to) public payable {
+    (bool success, ) = _to.call.value(msg.value)("");
+    require(success, "Ether transfer failed");
     }
 }
