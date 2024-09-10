2cd7f11e6ed89ce8c0d69004cbeb77eff2570fa6
Monica Jin <monicachenjin@gmail.com>
2024-09-10 09:35:51 +0200

    add exploit

--- smartbugs-curated/0.4.x/test/unchecked_low_level_calls/0x3f2ef511aa6e75231e4deafc7a3d2ecab3741de2_test.js
+++ smartbugs-curated/0.4.x/test/unchecked_low_level_calls/0x3f2ef511aa6e75231e4deafc7a3d2ecab3741de2_test.js
@@ -1,13 +1,13 @@
 const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
 const { expect } = require('chai');
 
-describe("attack unchecked_low_level_calls/0x3e013fc32a54c4c5b6991ba539dcd0ec4355c859.sol", function () {
+describe("attack unchecked_low_level_calls/0x3f2ef511aa6e75231e4deafc7a3d2ecab3741de2.sol", function () {
 
   let onwer;
 
   async function deployContracts() {
     [owner] = await ethers.getSigners();
-    const WhaleGiveaway2 = await ethers.getContractFactory("contracts/dataset/unchecked_low_level_calls/0x3e013fc32a54c4c5b6991ba539dcd0ec4355c859.sol:WhaleGiveaway2");
+    const WhaleGiveaway2 = await ethers.getContractFactory("contracts/dataset/unchecked_low_level_calls/0x3f2ef511aa6e75231e4deafc7a3d2ecab3741de2.sol:WhaleGiveaway2");
     const contract = await WhaleGiveaway2.connect(owner).deploy();
 
     const RevertContract = await ethers.getContractFactory("contracts/unchecked_low_level_calls/revert_contract.sol:RevertContract");
