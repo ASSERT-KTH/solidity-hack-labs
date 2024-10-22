044d0cd2fa8962aa8ffd2598c8444cae79d8e418
Monica Jin <monicachenjin@gmail.com>
2024-10-22 19:20:26 +0200

    add benign contract for sanity check

---
+++ smartbugs-curated/0.4.x/contracts/reentrancy/modifier_reentrancy_benign.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.4.24;
+
+import "../dataset/reentrancy/modifier_reentrancy.sol";
+
+contract BankBenign {
+    ModifierEntrancy modifierEntrancyInstance;
+
+    constructor(address _victimAddress) public {
+        modifierEntrancyInstance = ModifierEntrancy(_victimAddress);
+    }
+
+    function supportsToken() external pure returns(bytes32){
+        return(keccak256(abi.encodePacked("Nu Token")));
+    }
+
+    function airDrop() public {
+        modifierEntrancyInstance.airDrop();
+    }
+}
